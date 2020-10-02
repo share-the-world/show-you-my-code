d4f828cece9e6972225ec9467a4a182cf9877f04
shiyuan2he <shiyuan4work@126.com>
2020-10-02 19:44:42 +0800

    build

---
+++ java/0001-jia/src/main/java/org/develop/algorithm/App.java
@@ -0,0 +1,14 @@
+package org.develop.algorithm;
+
+/**
+ * Hello world!
+ *
+ * @author heshiyuan
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
