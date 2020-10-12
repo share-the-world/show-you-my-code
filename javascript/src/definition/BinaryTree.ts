53e99c4bc584aaf831677f9c5c43ea1a3fdba840
吴超 <wuchao@zhongbaounion.com>
2020-10-12 14:30:39 +0800

    <第14天>:递归

--- javascript/src/definition/BinaryTree.ts
+++ javascript/src/definition/BinaryTree.ts
@@ -1,24 +1,10 @@
 
+export type BinaryTreeType = BinaryTree | null | undefined;
 // 二叉树
 export interface BinaryTree {
   val: number
-  left?: BinaryTree
-  right?: BinaryTree
-}
-
-// 测试数据
-export const treeNode: BinaryTree = {
-  val:12,
-  left: {
-    val:1,
-    left:{ val: 2},
-    right: { val: 3}
-  },
-  right: {
-    val:7,
-    left: { val: 4},
-    right: { val: 5}
-  }
+  left?: BinaryTreeType
+  right?: BinaryTreeType
 }
 
 
