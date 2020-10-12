6066ccf1b7841a61871e7c8f4aedd8a63f5ce77c
吴超 <wuchao@zhongbaounion.com>
2020-10-12 20:16:15 +0800

    <第14天>:题目未完成

--- javascript/src/code/014/index.ts
+++ javascript/src/code/014/index.ts
@@ -1,24 +1,30 @@
-import { BinaryTreeType } from './../../definition/BinaryTree';
-import { BinaryTree } from '../../definition'
+import { BinaryTree, BinaryTreeType } from '../../definition'
 import {getTreeCase1,treeNodeCase2, treeNodeCase3} from './TestCase'
+import { bubbleSort } from '../../utils/sort/BubbleSort';
 
-const getMinimumDifference = (root: BinaryTree) => {
-  const arr: number[] = []
-  toArray1(root,arr);
-  console.log('arr1:',arr)
-}
-const toArray = (node?: BinaryTree | null): number[] =>  {
-  if (!node) return []
-  const nums: number[] = [node.val];
-  let left = node?.left;
-  let right = node?.right;
-  while(left || right){
-    left && nums.push(left!.val);
-    right && nums.push(right!.val);
-    left = left?.left;
-    right = right?.right;
+//  二叉搜索树的最小绝对差
+const getMinimumDifference = (root: BinaryTree): number => {
+  const arr: number[] = [];
+  const loop = (node: BinaryTreeType, nums: number[]) => {
+    if (!node) return;
+    nums.push(node.val)
+    loop(node.left, nums);
+    loop(node.right, nums);
   }
-  return nums;
+  loop(root, arr)
+  bubbleSort(arr)
+  const newArr = reduceNum(arr)
+  bubbleSort(newArr)
+  console.log('减法的数组：',newArr)
+  return newArr[0];
+}
+// 相邻的两个数，两两相见，差村委新数组
+const reduceNum = (arr: number[]): number[] => {
+  const res: number[] = []
+  for (let i = 0; i < arr.length - 1; i++) {
+    res.push(arr[i+1] - arr[i])
+  }
+  return res;
 }
 
 // 递归遍历-先序-根左右
@@ -37,7 +43,10 @@ const toArray2 = (node: BinaryTreeType, nums: number[]): void =>  {
   toArray1(node.right, nums);
 }
 
-getMinimumDifference(treeNodeCase2)
-getMinimumDifference(treeNodeCase3)
+// 执行测试: yarn run ts-node src/code/014/index.ts
+(() => {
+  const res = getMinimumDifference(treeNodeCase2)
+})()
+
 
 
