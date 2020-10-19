c099ef44d7a006c2aea73ec9790f2b05aa1cc44b
shiyuan2he <shiyuan4work@126.com>
2020-10-19 19:52:51 +0800

    build

--- java/0016-jia/src/main/java/org/develop/algorithm/App.java
+++ java/0016-jia/src/main/java/org/develop/algorithm/App.java
@@ -1,9 +1,8 @@
 package org.develop.algorithm;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Hello world!
@@ -11,28 +10,53 @@ import java.util.Map;
  * @author heshiyuan
  */
 public class App {
-    public void soluation( String[] args ){
-        Map<Character, Integer> map = new HashMap<>(150);
-        for(String arg : args){
-            char[] charArray = arg.toCharArray();
-            if(null!=charArray&&charArray.length>0){
-                for (int i = 0; i < charArray.length; i++) {
-                   if(null!=map.get(charArray[i])){
-                        map.put(charArray[i], map.get(charArray[i]) + 1);
-                   } else {
-                        map.put(charArray[i], 1); 
-                   }
-                }
-            }
+
+    /**
+     * args是由小写字母组成的数组
+     * 思路： 
+     * 1. 分桶：args中一个元素一个桶（hash，或者数组实现）
+     * 2. 统计：统计每个桶中每个字母出现的数量
+     * 3. 合并：取每个桶中相同字母中的最小值
+     */
+    public List<String> soluation( String[] args ){
+        // 1. 数组实现桶
+        return doItByArray(args);
+    }
+    /**
+     * {"bella", "label", "roller"};
+     *  [1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        [1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0]
+     * @param args
+     */
+    private List<String> doItByArray(String[] args) {
+        char[][] charArray = new char[args.length][26];
+
+        for (int i = 0; i < args.length; i++) {
+            char[] firstLine = args[i].toCharArray();
+            for (int j = 0; j < firstLine.length; j++) {
+                int index = firstLine[j] - 'a';
+                charArray[i][index] = (char)(firstLine[index] + 1);
+                System.out.println("000=" + charArray[i][index]);
+            } 
         }
+       
         List<String> returnList = new ArrayList<>();
-        for(Character key : map.keySet()){
-            if(null != map.get(key)&& map.get(key) % args.length == 0){
-                for (int i = 0; i < map.get(key)/args.length ; i++) {
-                    returnList.add(key.toString());
-                }
-            }
-        }
-        System.out.println(returnList.toString());
+
+        // for (int i = 0; i < charArray[0].length; i++) {
+        //     int min = Integer.MAX_VALUE;
+        //     for (int j = 0; j < args.length; j++) {
+        //         if(charArray[j][i]>0 && charArray[j][i] < min){
+        //             min = charArray[j][i];
+        //         } 
+        //     }
+        //     if(min == Integer.MAX_VALUE){
+        //         continue;
+        //     }
+        //     for (int j = 0; j < min; j++) {
+        //         returnList.add(String.valueOf((char) (min + 'a'))) ;
+        //     }
+        // }
+        return returnList;
     }
 }
--- java/0016-jia/src/test/java/org/develop/algorithm/AppTest.java
+++ java/0016-jia/src/test/java/org/develop/algorithm/AppTest.java
@@ -1,20 +1,19 @@
 package org.develop.algorithm;
 
+import java.util.List;
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
     public void shouldAnswerWithTrue(){
         App app = new App();
-        // ["cool","lock","cook"]
         String[] args = {"bella", "label", "roller"};
-        app.soluation(args);    
+        // String[] args = {"cool","lock","cook"};
+        List<String> returnArray = app.soluation(args);    
+        System.out.println("2222=" + returnArray.toString()); 
     }
 }
