edeb3e04d040be1ba6b000c8a90aa2fc691a0840
吴超 <wuchao@zhongbaounion.com>
2020-10-25 01:26:07 +0800

    <第27天>:上传题目,并解题

---
+++ javascript/__tests__/test_027.ts
@@ -0,0 +1,13 @@
+import {longestMountain} from '../src/code/027'
+
+
+const case1 = [2,1,4,7,3,2,5];
+const case2 = [2,2,2];
+const case3 = [0,1,2,3,4,5,4,3,2,1,0];
+const case4 = [0,1,2,3,4,5,6,7,8,9];
+
+// 执行测试: yarn run ts-node __tests__/test_027.ts
+(()=>{
+  // longestMountain(case1)
+  longestMountain(case4)
+})()
