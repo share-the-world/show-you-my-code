bcd567130f783acc2784f51265c52711f722d4ba
吴超 <wuchao@zhongbaounion.com>
2020-10-27 11:51:13 +0800

    <第29天>:二叉树前序遍历,迭代法

--- javascript/src/utils/tree/TreeArray.ts
+++ javascript/src/utils/tree/TreeArray.ts
@@ -38,7 +38,7 @@ export const array2BinaryTree = (arr: ArrayType): BinaryTree => {
   const length = arr.length;
   list.length = length;// 定义好数组长度,元素全是null
   const halfLen = Math.floor(arr.length / 2);// 数组长度的一半，保留整数
-  console.log('halfLen:',halfLen)
+  // console.log('halfLen:',halfLen)
   for (let i = 0; i <= halfLen; i++) {
     if (isExist(arr[i])) { // 元素存在，说明不是空节点
       if (!list[i]) {// 如果是null，就要添加新节点
@@ -46,7 +46,7 @@ export const array2BinaryTree = (arr: ArrayType): BinaryTree => {
       }
       let leftIndex = i*2+1;// 左子树位置
       let rightIndex = i*2+2;// 右子树位置
-      console.log(`i=${i}, node=${JSON.stringify(list[i])}, length=${list.length}`)
+      // console.log(`i=${i}, node=${JSON.stringify(list[i])}, length=${list.length}`)
       if (isExist(arr[leftIndex])) {
         (list[i]!.left = { val: Number(arr[leftIndex]) });// 奇数为左
         list[leftIndex] = list[i]!.left;// leftIndex位置上赋值左节点
@@ -56,13 +56,13 @@ export const array2BinaryTree = (arr: ArrayType): BinaryTree => {
         list[rightIndex] = list[i]!.right;// leftIndex位置上赋值右节点
       }
 
-      console.log(`leftIndex左 = ${leftIndex},${arr[leftIndex]}`)
-      console.log(`rightIndex右 = ${rightIndex},${arr[rightIndex]}`)
-      console.log(`i=${i}, node=${JSON.stringify(list[i])}, length=${list.length}`)
-      console.log('_____________________________________')
+      // console.log(`leftIndex左 = ${leftIndex},${arr[leftIndex]}`)
+      // console.log(`rightIndex右 = ${rightIndex},${arr[rightIndex]}`)
+      // console.log(`i=${i}, node=${JSON.stringify(list[i])}, length=${list.length}`)
+      // console.log('_____________________________________')
     }
   }
-  console.log('二叉树：',JSON.stringify(list[0]))
+  // console.log('二叉树：',JSON.stringify(list[0]))
   return list[0]!;// 头结点
 }
 // 存在并且为数字类型
