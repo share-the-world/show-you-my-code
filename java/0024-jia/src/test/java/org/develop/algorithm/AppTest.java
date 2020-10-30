55da3d778458e30a949fec240d0a907730c6ab39
shiyuan2he <shiyuan4work@126.com>
2020-10-30 15:45:59 +0800

    build

---
+++ java/0024-jia/src/test/java/org/develop/algorithm/AppTest.java
@@ -0,0 +1,12 @@
+package org.develop.algorithm;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    App app = new App();
+    
+    @Test
+    public void testDoIt(){
+        app.doIt("ababcbacadefegdehijhklij");
+    }
+}
