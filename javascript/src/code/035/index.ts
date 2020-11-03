604aed38a234364012f5597e7273fb5b84fc53e4
吴超 <wuchao@zhongbaounion.com>
2020-11-03 14:47:15 +0800

    <第36天>:采用左右指针

---
+++ javascript/src/code/035/index.ts
@@ -0,0 +1,5 @@
+const intersection = (nums1: number[], nums2: number[]): number[] => {
+  const set = new Set(nums1);
+  Array.from(set)
+  return [];
+}
---
+++ javascript/src/code/036/index.ts
@@ -0,0 +1,25 @@
+/**
+ * 有效的山脉数组
+ * https://leetcode-cn.com/problems/valid-mountain-array/
+ * @param A 
+ */
+export const validMountainArray = (A: number[]): boolean => {
+  for(let i = 0; i < A.length; i ++) {
+    let top = true;
+    for (let l = 0; l < i; l ++) {
+      if (A[l] >= A[l+1]) {// 如果相邻两个，左>=右，说明[l]不是峰顶
+        top = false;
+        break;
+      }
+    }
+    if (!top) continue;// 左侧无顶峰，直接进入下次循环
+    for (let r = A.length-1; r > i; r --) {// 如果相邻两个，右>=左，说明[r]不是峰顶
+      if (A[r] >= A[r-1]) {
+        top = false;
+        break;
+      }
+    }
+    if (top && i !== 0 && i !== A.length-1) return true;
+  }
+  return false;
+}
