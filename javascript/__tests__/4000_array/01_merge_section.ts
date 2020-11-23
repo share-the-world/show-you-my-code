2a9ea5c4e20d2953967ff43db520f7d12715154c
吴超 <541847041@qq.com>
2020-11-23 16:20:11 +0800

    <数组>:合并区间,双重循环

--- javascript/__tests__/4000_array/01_merge_section.ts
+++ javascript/__tests__/4000_array/01_merge_section.ts
@@ -1,12 +1,15 @@
-import {merge1} from '../../src/code/4000_array/01_mergeSection'
-import { case5 } from '../tree/case';
+import {merge} from '../../src/code/4000_array/01_mergeSection1'
 
-const case1 = [[1,3],[2,6],[8,10],[15,18]];
-const case2 = [[1,3],[2,6],[8,10],[15,18],[1,8],[14,20]];
-const case3 = [[1,4],[0,4]];
-const case4 = [[1,4],[2,3]];
+// const case1 = [[1,3],[2,6],[8,10],[15,18]];
+// const case2 = [[1,3],[2,6],[8,10],[15,18],[1,8],[14,20]];
+// const case3 = [[1,4],[0,4]];
+// const case4 = [[1,4],[2,3]];
+const case5: number[][] = [];
 
-// 执行测试：
+// 执行测试：yarn run ts-node __tests__/4000_array/01_merge_section.ts
 (() => {
-  merge1(case4);
+  // merge(case1);
+  // merge(case2);
+  // merge(case3);
+  merge(case5);
 })()
