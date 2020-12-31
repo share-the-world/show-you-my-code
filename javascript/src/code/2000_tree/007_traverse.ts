561043f1a70a057383e4b20ec067e79d3cfdf6d6
吴超 <541847041@qq.com>
2020-12-31 10:04:56 +0800

    <第91天>:未来=完成

---
+++ javascript/src/code/2000_tree/007_traverse.ts
@@ -0,0 +1,12 @@
+import { BinaryTree, BinaryTreeType } from '../../definition'
+
+
+/**
+ * 103. 二叉树的锯齿形层序遍历
+ * https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/
+ */
+export const zigzagLevelOrder =(root: BinaryTree): number[][] => {
+  const arr: number[][] = [];
+  
+  return [];
+}
