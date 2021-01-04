55c9980a7871c17a983490d36aa819a8d4d08287
吴超 <541847041@qq.com>
2021-01-04 11:11:04 +0800

    <97天>:上传题目

---
+++ javascript/src/code/097/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 605. 种花问题
+ * https://leetcode-cn.com/problems/can-place-flowers/
+ * @param flowerbed 
+ * @param n 
+ */
+export const canPlaceFlowers = (flowerbed: number[], n: number): boolean => {
+    return false;
+}
