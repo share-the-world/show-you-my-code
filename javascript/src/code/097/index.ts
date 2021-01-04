4efc81b88ce255af2f7a32f34af0eb491cd92207
吴超 <541847041@qq.com>
2021-01-04 13:34:36 +0800

    <第97天>:数组-窗口

--- javascript/src/code/097/index.ts
+++ javascript/src/code/097/index.ts
@@ -5,5 +5,19 @@
  * @param n 
  */
 export const canPlaceFlowers = (flowerbed: number[], n: number): boolean => {
+    let i = 0, count = n;
+    while(n > 0 && i < flowerbed.length) {
+        if (flowerbed[i] === 0 && 
+            (flowerbed[i -1] === 0 || flowerbed[i -1] === undefined) && 
+            (flowerbed[i +1] === 0 || flowerbed[i +1] === undefined)) {
+            flowerbed[i] = 1;
+            count --;
+        }
+        if (count === 0) return true
+        i ++;
+    }
     return false;
 }
+// yarn run ts-node src/code/097/index.ts 
+console.log('结果=', canPlaceFlowers([1,0,1,0,1,0,0,0,0], 1))
+console.log('结果=', canPlaceFlowers([0,0,1,0,0], 2))
