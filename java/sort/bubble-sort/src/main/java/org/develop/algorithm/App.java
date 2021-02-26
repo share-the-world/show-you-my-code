6af967c13b81ea8372835bd8a4b4252ece5470d5
shiyuan2he <shiyuan4work@126.com>
2021-02-26 16:58:44 +0800

    冒泡排序之鸡尾酒排序

--- java/sort/bubble-sort/src/main/java/org/develop/algorithm/App.java
+++ java/sort/bubble-sort/src/main/java/org/develop/algorithm/App.java
@@ -28,11 +28,49 @@ public class App {
                     flag = 1;
                 }
             }
+            // 提前排好序时，跳出循环
             if(flag == 0){
                 break;
             }
         }
         System.out.println(count + "    " + swapCount);
         return nums;
+    }
+
+    public int[] cocktail(int[] nums){
+        if (null == nums || nums.length == 0 || nums.length == 1) {
+            System.out.println("0   0");
+            return nums;
+        } 
+        int left = 0, right = nums.length - 1;
+        int count=0, swapCount=0;
+        while(left < right){
+            //先从右到左，把最大的找出来
+            for (int i = left; i < right; i++) {
+                count ++;
+                if(nums[i] > nums[i+1]){
+                    swapCount ++;
+                    swap(nums, i, i+1);
+                }
+            }
+            right --;
+            // 从左到右，将最小的放到左边
+            for (int i = right; i > left; i--) {
+                count ++;
+                if(nums[i-1] > nums[i]){
+                    swapCount ++;
+                    swap(nums, i-1, i);
+                }
+            }
+            left ++;
+        }
+        System.out.println(count + "    " + swapCount);
+        return nums;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
     }
 }
