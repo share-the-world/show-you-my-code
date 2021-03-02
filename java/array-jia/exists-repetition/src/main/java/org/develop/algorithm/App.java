4ffab149ed9f929f2dc11802afdef743000df60c
shiyuan2he <shiyuan4work@126.com>
2021-03-02 16:16:58 +0800

    <217. 存在重复元素>

--- java/array-jia/exists-repetition/src/main/java/org/develop/algorithm/App.java
+++ java/array-jia/exists-repetition/src/main/java/org/develop/algorithm/App.java
@@ -1,13 +1,32 @@
 package org.develop.algorithm;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  * Hello world!
- *
+ * @author hsy
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App{
+    public boolean existRepetition(int[] args){
+        if(null == args || args.length == 0){
+            return false;
+        }
+        return IntStream.of(args).distinct().count() != args.length;
+    }
+
+    public boolean existRepetition2(int[] args){
+        if(null == args || args.length == 0){
+            return false;
+        }
+        // 先排序
+        Arrays.sort(args);
+        // 判断连续的两个元素相等，
+        for(int i=0; i<args.length-1; i++){
+            if(args[i] == args[i+1]){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- java/array-jia/exists-repetition/src/test/java/org/develop/algorithm/AppTest.java
+++ java/array-jia/exists-repetition/src/test/java/org/develop/algorithm/AppTest.java
@@ -1,38 +1,28 @@
 package org.develop.algorithm;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.logging.Logger;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest{
+    Logger logger = Logger.getLogger(AppTest.class.getName());
+    App app = new App();
+    @org.junit.Test
+    public void testExistRepetition(){
+        logger.info("" + app.existRepetition(new int[]{}));
+        logger.info("" + app.existRepetition(new int[]{1,2,3,1}));
+        logger.info("" + app.existRepetition(new int[]{1,2,3,4}));
+        logger.info("" + app.existRepetition(new int[]{1,1,1,3,3,4,3,2,4,2}));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testExistRepetition2(){
+        logger.info("" + app.existRepetition2(new int[]{}));
+        logger.info("" + app.existRepetition2(new int[]{1,2,3,1}));
+        logger.info("" + app.existRepetition2(new int[]{1,2,3,4}));
+        logger.info("" + app.existRepetition2(new int[]{1,1,1,3,3,4,3,2,4,2}));
     }
 }
