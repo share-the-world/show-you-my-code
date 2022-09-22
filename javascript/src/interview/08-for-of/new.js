441dd4a4e220796d4ee0d36f8515cf12bc553d26
wuchao <wuchao@shiqiao.com>
2022-09-22 09:51:38 +0800

    feat: 🎸 for-of实现

---
+++ javascript/src/interview/08-for-of/new.js
@@ -0,0 +1,13 @@
+function ListNode(value) {
+  this.value = value;
+  this.next = ListNode
+}
+
+
+const node1 = ListNode(1)
+const node2 = new ListNode(2)
+
+console.log(node1)
+console.log(node2)
+
+// node javascript/src/interview/08-for-of/new.js
