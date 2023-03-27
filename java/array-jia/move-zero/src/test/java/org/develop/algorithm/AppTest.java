5ff8ac8a21f7a202d307b7d22304b1d509f0c58a
shiyuan2he <shiyuan4work@126.com>
2023-03-27 22:45:25 +0800

    move zero

--- java/array-jia/move-zero/src/test/java/org/develop/algorithm/AppTest.java
+++ java/array-jia/move-zero/src/test/java/org/develop/algorithm/AppTest.java
@@ -1,25 +1,21 @@
 package org.develop.algorithm;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.Arrays;
-import java.util.logging.Logger;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    Logger logger = Logger.getLogger(AppTest.class.getName());
     App app = new App();
+
     @Test
-    public void moveZero(){
-        logger.info("" + Arrays.toString(app.moveZero(new int[]{0,1,0,3,12})));
-        logger.info("" + Arrays.toString(app.moveZero(new int[]{0})));
-        logger.info("" + Arrays.toString(app.moveZero(new int[]{1})));
-        logger.info("" + Arrays.toString(app.moveZero(new int[]{0,0, 0})));
-        logger.info("" + Arrays.toString(app.moveZero(new int[]{1, 1, 1, 3})));
-        logger.info("" + Arrays.toString(app.moveZero(new int[]{0, 0 , 2, 23, 0, 0})));
+    public void moveZero() {
+        Assert.assertArrayEquals(new int[]{1, 3, 12, 0, 0}, app.moveZero(new int[]{0, 1, 0, 3, 12}));
+        Assert.assertArrayEquals(new int[]{0}, app.moveZero(new int[]{0}));
+        Assert.assertArrayEquals(new int[]{1}, app.moveZero(new int[]{1}));
+        Assert.assertArrayEquals(new int[]{0, 0, 0}, app.moveZero(new int[]{0, 0, 0}));
+        Assert.assertArrayEquals(new int[]{1, 1, 1, 3}, app.moveZero(new int[]{1, 1, 1, 3}));
+        Assert.assertArrayEquals(new int[]{2, 23, 0, 0, 0, 0}, app.moveZero(new int[]{0, 0, 2, 23, 0, 0}));
     }
 }
